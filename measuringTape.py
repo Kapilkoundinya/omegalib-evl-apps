e556c56a92f8c6da62f8a945d27dd563f9205137
Alessandro Febretti <febret@gmail.com>
2013-06-27 16:40:36 -0500

    NEW: measuring tape module (WIP)

---
+++ measuringTape.py
@@ -0,0 +1,72 @@
+# A measuring tape tool with movable ends.
+from math import *
+from euclid import *
+from omega import *
+from omegaToolkit import *
+from cyclops import *
+
+class MeasuringTape(Actor):
+	startHandle = None
+	startText = None
+	endHandle = None
+	endText = None
+	body = None
+	bodyLine = None
+	bodyText = None
+	interactor = None
+	
+	#--------------------------------------------------------------------------
+	def __init__(self):
+		super(MeasuringTape, self).__init__("measuringTape")
+		
+		self.startHandle = SphereShape.create(0.1, 2)
+		self.endHandle = SphereShape.create(0.1, 2)
+		self.startText = Text3D.create('fonts/arial.ttf', 0.05, 'start')
+		self.startHandle.addChild(self.startText)
+		self.endText = Text3D.create('fonts/arial.ttf', 0.05, 'end')
+		self.endHandle.addChild(self.endText)
+		self.body = LineSet.create()
+		self.bodyLine = self.body.addLine()
+		self.bodyLine.setThickness(0.1)
+		self.bodyText = Text3D.create('fonts/arial.ttf', 0.1, 'length')
+		
+		self.setEventsEnabled(True)
+		self.setUpdateEnabled(True)
+		
+		self.interactor = ToolkitUtils.setupInteractor('config/interactor')
+		
+		# set initial handle positions.
+		self.startHandle.setPosition(Vector3(-1,2,-4))
+		self.endHandle.setPosition(Vector3(1,2,-4))
+		
+		# Set visual properties
+		self.startHandle.setEffect('colored -e blue')
+		self.endHandle.setEffect('colored -e blue')
+		self.body.setEffect('colored -e #000060')
+		self.startText.getMaterial().setDepthTestEnabled(False)
+		self.endText.getMaterial().setDepthTestEnabled(False)
+		#self.endText.setFacingCamera(getDefaultCamera())
+		
+	#--------------------------------------------------------------------------
+	def dispose(self):
+		print('Deleting measuring tape')
+		
+	#--------------------------------------------------------------------------
+	def onUpdate(self, frame, time, dt):
+		#self.startText.setFacingCamera(getDefaultCamera())
+		self.startText.setText(str(self.startHandle.getPosition()))
+		self.endText.setText(str(self.endHandle.getPosition()))
+		self.bodyLine.setStart(self.startHandle.getPosition())
+		self.bodyLine.setEnd(self.endHandle.getPosition())
+		
+	#--------------------------------------------------------------------------
+	def onEvent(self):
+		e = getEvent()
+		if(e.isButtonDown(EventFlags.Button1)):
+			r = getRayFromEvent(e)
+			hitData = hitNode(self.startHandle, r[1], r[2])
+			if(hitData[0]):
+				self.interactor.setSceneNode(self.startHandle)
+			hitData = hitNode(self.endHandle, r[1], r[2])
+			if(hitData[0]):
+				self.interactor.setSceneNode(self.endHandle)
